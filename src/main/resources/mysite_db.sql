27d63cbb53ef666793b26bc308c897d288d424d0
Zoey <1358659056@qq.com>
2022-10-03 17:52:50 +0800

    feat: SQL - 博客文章表表

--- src/main/resources/mysite_db.sql
+++ src/main/resources/mysite_db.sql
@@ -16,3 +16,18 @@ CREATE TABLE IF NOT EXISTS `user` (
     `updated_time` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
 	PRIMARY KEY ( `id` )
 ) ENGINE = INNODB DEFAULT CHARSET = utf8;
+
+-- create content_article table by zoey he
+CREATE TABLE IF NOT EXISTS `content_article` (
+	`id` bigint(19) UNSIGNED AUTO_INCREMENT,
+	`user_id` bigint(19) NOT NULL COMMENT '作者ID',
+	`author` VARCHAR(20) NOT NULL COMMENT '作者',
+	`title` VARCHAR(255) NOT NULL COMMENT '文章标题',
+	`content` longtext NOT NULL COMMENT '文章正文',
+	`category` int(2) COMMENT '文章分类, 两位: 一号位 {1: 原创, 2: 转载}, 二号位{0: 技术, 1: 游戏, 2: 旅行, 3: 其他}',
+	`label` VARCHAR(255) COMMENT '文章标签',
+	`deleted` tinyint(1) DEFAULT 0 NOT NULL COMMENT '0: 否 1: 是',
+	`created_time` timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_time` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+	PRIMARY KEY ( `id` )
+) ENGINE = INNODB DEFAULT CHARSET = utf8;
