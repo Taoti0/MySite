6cad411dc21e0a9db036872a05748e307e5c8fee
Zoey <1358659056@qq.com>
2022-10-03 17:52:14 +0800

    feat: SQL - 用户表

--- src/main/resources/mysite_db.sql
+++ src/main/resources/mysite_db.sql
@@ -5,14 +5,14 @@ DEFAULT COLLATE utf8_general_ci;
 
 -- create user table by zoey he
 CREATE TABLE IF NOT EXISTS `user` (
-	`id` INT(19) UNSIGNED AUTO_INCREMENT,
-	`username` VARCHAR (255) UNIQUE NOT NULL COMMENT '用户名',
-	`password` VARCHAR (255) NOT NULL COMMENT '密码',
-	`nickname` VARCHAR (255) COMMENT '昵称',
-	`sex` tinyint (1) COMMENT '0: 无性别 1: 男 2: 女',
-	`age` INT (12) COMMENT '年龄',
-	`deleted` tinyint (1) DEFAULT 0 NOT NULL COMMENT '0: 否 1: 是',
-	`createdTime` timestamp COMMENT '创建时间',
-	`updatedTime` timestamp COMMENT '修改时间',
+	`id` bigint(19) UNSIGNED AUTO_INCREMENT,
+	`username` VARCHAR(255) UNIQUE NOT NULL COMMENT '用户名',
+	`password` VARCHAR(255) NOT NULL COMMENT '密码',
+	`nickname` VARCHAR(255) COMMENT '昵称',
+	`sex` tinyint(1) DEFAULT 0 COMMENT '0: 无性别 1: 男 2: 女',
+	`age` INT(11) COMMENT '年龄',
+	`deleted` tinyint(1) DEFAULT 0 NOT NULL COMMENT '0: 否 1: 是',
+	`created_time` timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_time` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
 	PRIMARY KEY ( `id` )
 ) ENGINE = INNODB DEFAULT CHARSET = utf8;
