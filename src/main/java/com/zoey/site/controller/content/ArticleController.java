6d112c0c15ac09c8fd96e6f5a3d39a5763a5a21a
Zoey <1358659056@qq.com>
2022-10-14 14:54:08 +0800

    fix: 修改访问路径

--- src/main/java/com/zoey/site/controller/content/ArticleController.java
+++ src/main/java/com/zoey/site/controller/content/ArticleController.java
@@ -20,19 +20,19 @@ import java.util.List;
  * @data: 2022-10-02 13:21
  * @Description 内容 - 文章管理
  */
-@RestController("/content/article")
+@RestController("/content")
 @Api(tags = "内容 - 文章管理")
 public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    @PostMapping("/add")
+    @PostMapping("/article/add")
     @ApiOperation(value = "新增文章", notes = "新增文章")
     public Result<Boolean> add(@RequestBody ArticleForm form) {
         return Result.success(articleService.add(form.toPo(Article.class)));
     }
 
-    @GetMapping("/page")
+    @GetMapping("/article/page")
     @ApiOperation(value = "获取所有文章", notes = "获取所有文章 page")
     public Result<IPage<Article>> page(String userId, Integer currentPage, Integer pageSize) {
         Page<Article> page = new Page<>(currentPage, pageSize);
@@ -40,25 +40,25 @@ public class ArticleController {
         return Result.success(page);
     }
 
-    @GetMapping("/list")
+    @GetMapping("/article/list")
     @ApiOperation(value = "获取所有文章", notes = "获取所有文章 list")
     public Result<List<Article>> list(String userId){
         return Result.success(articleService.getList(userId));
     }
 
-    @GetMapping("/get")
+    @GetMapping("/article/get")
     @ApiOperation(value = "获取指定文章", notes = "获取指定文章")
     public Result<Article> get(Long id){
         return Result.success(articleService.get(id));
     }
 
-    @PostMapping("/updateArticle")
+    @PostMapping("/article/update")
     @ApiOperation(value = "修改文章", notes = "修改文章")
     public Result<Boolean> update(@RequestBody ArticleUpdateForm form) {
         return Result.success(articleService.update(form));
     }
 
-    @DeleteMapping("/removeArticle")
+    @DeleteMapping("/article/remove")
     @ApiOperation(value = "删除指定文章", notes = "删除指定文章")
     public Result<Boolean> remove(Long id) {
         return Result.success(articleService.removeById(id));
